c110530205a36daa1e0dcea08742404a89d8f9ee
Andrea Bertrini <bertarini45@gmail.com>
2015-06-05 17:30:03 +0200

    Kinematic and ToNAo added. Non funziona perfettamente

--- NAOKinect/Kinematic.cs
+++ NAOKinect/Kinematic.cs
@@ -13,31 +13,89 @@ namespace NAOKinect
         /// Receive a rotation matrix of type Matrix4
         /// Return a Vector3Float containing the rotation around the 3 axis
         ///</summary>
-        public static Vecto3Float computeEulerFromMatrix(Matrix4 m)
+        public static Vecto3Float computeEulerFromMatrixXYZ(Matrix4 m)
         {
             Vecto3Float sol = new Vecto3Float();
             if (m.M13 < 1f)
             {
                 if (m.M13 > -1f)
                 {
-                    sol.X = (float)Math.Atan2((double)-m.M23, (double)m.M33);
-                    sol.Y = (float)Math.Asin((double)m.M13);
-                    sol.Z = (float)Math.Atan2((double)-m.M12, (double)m.M11);
+                     sol.X = (float)Math.Atan2((double)-m.M23, (double)m.M33);
+                     sol.Y = (float)Math.Asin((double)m.M13);
+                     sol.Z = (float)Math.Atan2((double)-m.M12, (double)m.M11);
                 }
                 else
                 {
-                    sol.X = -(float)Math.Atan2((double)-m.M21, (double)m.M22);
-                    sol.Y = -(float)(Math.PI) / 2;
-                    sol.Z = 0f;
+                     sol.X = -(float)Math.Atan2((double)-m.M21, (double)m.M22);
+                     sol.Y = -(float)(Math.PI) / 2;
+                     sol.Z = 0f;
                 }
 
             }
             else
             {
-                sol.X = (float)Math.Atan2((double)-m.M21, (double)m.M22);
-                sol.Y = (float)(Math.PI) / 2;
-                sol.Z = 0f;
+                 sol.X = (float)Math.Atan2((double)-m.M21, (double)m.M22);
+                 sol.Y = (float)(Math.PI) / 2;
+                 sol.Z = 0f;
             }
+            return sol;
+        }
+
+        public static Vecto3Float computeEulerFromMatrixZXY(Matrix4 m)
+        {
+            Vecto3Float sol = new Vecto3Float();
+            if (m.M13 < 1f)
+            {
+                if (m.M13 > -1f)
+                {
+                    sol.X = (float)Math.Asin((double)m.M32);
+                    sol.Y = (float)Math.Atan2((double)-m.M12, (double)m.M22);
+                    sol.Z = (float)Math.Atan2((double)-m.M31, (double)m.M33);
+                }
+                else
+                {
+                    sol.X = -(float)(Math.PI) / 2;
+                    sol.Y = 0f;
+                    sol.Z = -(float)Math.Atan2((double)m.M13, (double)m.M11);
+                }
+
+            }
+            else
+            {
+                sol.X = (float)(Math.PI) / 2;
+                sol.Y = 0f;
+                sol.Z = -(float)Math.Atan2((double)m.M13, (double)m.M11);
+            }
+            return sol;
+        }
+
+        public static Vecto3Float computeEulerFromMatrixZYX(Matrix4 m)
+        {
+            Vecto3Float sol = new Vecto3Float();
+            if (m.M32 < +1f)
+            {
+                if (m.M32 > -1f)
+                {
+                     sol.X = (float)Math.Asin((double)m.M32);
+                     sol.Z = (float)Math.Atan2(-(double)m.M12, (double)m.M22);
+                     sol.Y = (float)Math.Atan2((double)m.M31, (double)m.M33);
+                }
+                else
+                {
+                     sol.X = (float)-Math.PI / 2;
+                     sol.Z = -(float)Math.Atan2((double)m.M13, (double)m.M11);
+                     sol.Y = 0f;
+                }
+
+            }
+            else
+            {
+                 sol.X = (float)Math.PI / 2;
+                 sol.Z = (float)Math.Atan2((double)m.M13, (double)m.M11);
+                 sol.Y = 0f;
+            }
+
+
             return sol;
         }
 
@@ -45,45 +103,46 @@ namespace NAOKinect
         /// Receive a rotation matrix of type Vector4
         /// Return a Vector3Float containing the rotation around the 3 axis
         ///</summary>
-        public static Vecto3Float computeEulerFromQuaternion(Vector4 q)
-        {
-
-            // Store the Euler angles in radians
-            Vecto3Float pitchYawRoll = new Vecto3Float();
-
-            double sqw = q.W * q.W;
-            double sqx = q.X * q.X;
-            double sqy = q.Y * q.Y;
-            double sqz = q.Z * q.Z;
-
-            // If quaternion is normalised the unit is one, otherwise it is the correction factor
-            double unit = sqx + sqy + sqz + sqw;
-            double test = q.X * q.Y + q.Z * q.W;
-
-            if (test > 0.4999f * unit)                              // 0.4999f OR 0.5f - EPSILON
+            public static Vecto3Float computeEulerFromQuaternion(Vector4 q)
             {
-                // Singularity at north pole
-                pitchYawRoll.Y = 2f * (float)Math.Atan2(q.X, q.W);  // Yaw
-                pitchYawRoll.X = (float)Math.PI * 0.5f;             // Pitch
-                pitchYawRoll.Z = 0f;                                // Roll
+
+                // Store the Euler angles in radians
+                Vecto3Float pitchYawRoll = new Vecto3Float();
+
+                double sqw = q.W * q.W;
+                double sqx = q.X * q.X;
+                double sqy = q.Y * q.Y;
+                double sqz = q.Z * q.Z;
+
+                // If quaternion is normalised the unit is one, otherwise it is the correction factor
+                double unit = sqx + sqy + sqz + sqw;
+                double test = q.X * q.Y + q.Z * q.W;
+
+                if (test > 0.4999f * unit)                              // 0.4999f OR 0.5f - EPSILON
+                {
+                    // Singularity at north pole
+                    pitchYawRoll.Z = 2f * (float)Math.Atan2(q.X, q.W);  // Yaw
+                    pitchYawRoll.Y = (float)Math.PI * 0.5f;             // Pitch
+                    pitchYawRoll.X = 0f;                                // Roll
+                    return pitchYawRoll;
+                }
+                else if (test < -0.4999f * unit)                        // -0.4999f OR -0.5f + EPSILON
+                {
+                    // Singularity at south pole
+                    pitchYawRoll.Z = -2f * (float)Math.Atan2(q.X, q.W); // Yaw
+                    pitchYawRoll.Y = (float)-Math.PI * 0.5f;            // Pitch
+                    pitchYawRoll.X = 0f;                                // Roll
+                    return pitchYawRoll;
+                }
+                else
+                {
+                    pitchYawRoll.Z = (float)Math.Atan2(2f * q.Y * q.W - 2f * q.X * q.Z, sqx - sqy - sqz + sqw);       // Yaw
+                    pitchYawRoll.Y = (float)Math.Asin(2f * test / unit);                                             // Pitch
+                    pitchYawRoll.X = (float)Math.Atan2(2f * q.X * q.W - 2f * q.Y * q.Z, -sqx + sqy - sqz + sqw);      // Roll
+                }
+
                 return pitchYawRoll;
             }
-            else if (test < -0.4999f * unit)                        // -0.4999f OR -0.5f + EPSILON
-            {
-                // Singularity at south pole
-                pitchYawRoll.Y = -2f * (float)Math.Atan2(q.X, q.W); // Yaw
-                pitchYawRoll.X = (float)-Math.PI * 0.5f;            // Pitch
-                pitchYawRoll.Z = 0f;                                // Roll
-                return pitchYawRoll;
-            }
-            else
-            {
-                pitchYawRoll.Y = (float)Math.Atan2(2f * q.Y * q.W - 2f * q.X * q.Z, sqx - sqy - sqz + sqw);       // Yaw
-                pitchYawRoll.X = (float)Math.Asin(2f * test / unit);                                             // Pitch
-                pitchYawRoll.Z = (float)Math.Atan2(2f * q.X * q.W - 2f * q.Y * q.Z, -sqx + sqy - sqz + sqw);      // Roll
-            }
-
-            return pitchYawRoll;
         }
     }
-}
+
---
+++ NAOKinect/ToNao.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aldebaran.Proxies;
+using Microsoft.Kinect;
+
+
+namespace NAOKinect
+{
+    class ToNao
+    {
+        private MotionProxy motioProxy;
+        private Aldebaran.Proxies.MemoryProxy memProxy;
+        private float fractionSpeed = 0.5f;
+
+
+        public ToNao(String IP, int port, Kinect kinect)
+        {
+            this.motioProxy = new MotionProxy(IP, port);
+            this.memProxy = new MemoryProxy(IP, port);
+            kinect.SkeletonReady += move;
+        }
+
+        public void setUpNao()
+        {
+             motioProxy.wakeUp();
+            //maybe some other thing
+        }
+
+        public void move(Skeleton[] skeletons)
+        {
+            //getting a tracked skeleton if any, else return a default one
+            Skeleton skel = (from trackskeleton in skeletons
+                             where trackskeleton.TrackingState == SkeletonTrackingState.Tracked
+                             select trackskeleton).FirstOrDefault();
+            if (skel != null)
+            {
+                //iterate on the skeleton found and "try" to move the NAO
+                foreach (BoneOrientation orientation in skel.BoneOrientations)
+                {
+                    //Vecto3Float eulerM1 = Kinematic.computeEulerFromMatrixZYX(orientation.HierarchicalRotation.Matrix);
+                    //Vecto3Float eulerQ = Kinematic.computeEulerFromQuaternion(orientation.HierarchicalRotation.Quaternion);
+                    //Vecto3Float eulerM2 = Kinematic.computeEulerFromMatrixXYZ(orientation.HierarchicalRotation.Matrix);
+                    Vecto3Float eulerM3 = Kinematic.computeEulerFromMatrixZXY(orientation.HierarchicalRotation.Matrix);
+                    this.setAngleFromOrientation(eulerM3, orientation.StartJoint,orientation.EndJoint);
+                }
+            }
+        }
+
+        public static float convertToRadians(int angle)
+        {
+            return (float) (angle* (Math.PI / 180));
+        }
+
+
+        private void setAngleFromOrientation(Vecto3Float rotation, JointType startJoint,JointType endJoint)
+        {
+
+            motioProxy.setStiffnesses("Body", 1.0f);
+            motioProxy.setSmartStiffnessEnabled(true);
+            if (startJoint == JointType.ShoulderRight)
+            {
+                
+                //motioProxy.setAngles(NAOJointName.RShoulderPitch, 0f fractionSpeed);
+                //motioProxy.setAngles(NAOJointName.RShouderRoll, 0f, fractionSpeed);
+            }
+            else if (startJoint == JointType.ShoulderLeft && endJoint == JointType.ElbowLeft)
+            {
+
+                motioProxy.setAngles(NAOJointName.LShoulderPitch, rotation.Y , fractionSpeed);
+                motioProxy.setAngles(NAOJointName.LShoulderRoll, rotation.X + 0.28f, fractionSpeed);
+                motioProxy.setAngles(NAOJointName.LElbowYaw, -rotation.Z , fractionSpeed);
+                
+            }
+            else if (startJoint == JointType.ElbowLeft && endJoint == JointType.WristLeft)
+            {
+                motioProxy.setAngles(NAOJointName.LElbowRoll, rotation.X , fractionSpeed);
+                motioProxy.setAngles(NAOJointName.LElbowYaw, -rotation.Z , fractionSpeed);
+            }
+            else if (startJoint == JointType.WristLeft)
+            {
+                //motioProxy.setAngles(NAOJointName.LWristYaw, rotation.Z, fractionSpeed);
+            }
+            else if (startJoint == JointType.ElbowRight && endJoint == JointType.WristRight)
+            {
+                //motioProxy.setAngles(NAOJointName.RElbowRoll, convertToRadians(-80), fractionSpeed);
+                //motioProxy.setAngles(NAOJointName.RElbowYaw, rotation.Z, fractionSpeed);
+            }
+            else if (startJoint == JointType.WristRight)
+            {
+                //motioProxy.setAngles(NAOJointName.RWristYaw, 0f, fractionSpeed);
+            }
+            else if (endJoint == JointType.Head)
+            {
+                motioProxy.setAngles(NAOJointName.HeadPitch, -rotation.X, fractionSpeed);
+                motioProxy.setAngles(NAOJointName.HeadYaw, rotation.Y, fractionSpeed);
+            }
+        }
+    }
+
+    class NAOJointName
+    {
+
+        public static String HeadPitch = "HeadPitch";
+        public static String HeadYaw = "HeadYaw";
+
+
+        public static String LShoulderRoll = "LShoulderRoll";
+        public static String LShoulderPitch = "LShoulderPitch";
+
+
+        public static String RShouderRoll = "RShoulderRoll";
+        public static String RShoulderPitch = "RShoulderPitch";
+
+        public static String LElbowYaw = "LElbowYaw";
+        public static String LElbowRoll = "LElbowRoll";
+
+
+        public static String RElbowYaw = "RElbowYaw";
+        public static String RElbowRoll = "RElbowRoll";
+
+
+        public static String LWristYaw = "LWristYaw";
+
+
+        public static String RWristYaw = "RWristYaw";
+
+
+        public static String LHipYaw = "LHipYaw";
+        public static String LHipPitch = "LHipPitch";
+        public static String LHipRoll = "LHipRoll";
+
+
+        public static String RHipYaw = "RHipYaw";
+        public static String RHipPitch = "RHipPitch";
+        public static String RHipRoll = "RHipRoll";
+
+
+        public static String LKneePitch = "LKneePitch";
+
+
+        public static String RKneePitch = "RKneePitch";
+
+
+        public static String LAnklePitch = "LAnklePitch";
+        public static String LAnkleRoll = "LAnkleRoll";
+
+
+        public static String RAnklePitch = "RAnklePitch";
+        public static String RAnkleRoll = "RAnkleRoll";
+    }
+}
